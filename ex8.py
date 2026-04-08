28326e58d938e900db1ce82b2a2e8ff01fc564c7
Rishiks pyhon <rishu.alluri@gmail.com>
2026-04-08 10:45:11 -0400

    Update ex8.py

---
+++ ex8.py
@@ -0,0 +1,9 @@
+w={
+    "03-03":"Rishik",
+    "01-06":"Chakrapani",
+    "08-16":"Sireesha",
+    "10-25":"Krithik"
+}
+h=input("Do you want to guess your freinds/families B-day")
+if h in w:
+    print("hooray",w[h],"'s Bday is That month+date")
