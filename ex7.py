dd9cf60d52a47555e5ab63ce0962246edfb77f25
Rishiks pyhon <rishu.alluri@gmail.com>
2026-04-08 10:22:42 -0400

    Lunch maker

---
+++ ex7.py
@@ -0,0 +1,4 @@
+w=input("Rice Wheat Corn (Maize) Potatoes Cassava Soybeans Sweet Potatoes Yams\n Plantains Sorghum Tomatoes Onions Cabbage Apples Bananas Chicken Eggs Milk/Dairy Pork Beef ")
+
+e=input("what drink do you want. Water, Tea, Coffee, Beer, Soft Drinks, Wine, Milk, Orange, Juice, Margarita, Martini, Old-\n Fashioned Espresso, Martini, Aperol, Spritz, Mojito, Gin, and Tonic, Moscow, Mule, Negroni, Lemonade, Kombucha, Hot Chocolate ")
+print("Here is your lunch:",w+' '+e)
